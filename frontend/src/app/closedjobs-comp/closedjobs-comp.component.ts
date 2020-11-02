9c5a8dfe44b2c1e27b34da20f5ef7cb5ea692eb2
Ayush Yadav <ayush911yadav@gmail.com>
2020-11-02 08:10:34 +0530

    updated closed jobs

--- frontend/src/app/closedjobs-comp/closedjobs-comp.component.ts
+++ frontend/src/app/closedjobs-comp/closedjobs-comp.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from '../job.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-closedjobs-comp',
